f1959df60e5d423ec59605393610ab0349650404
osamaelmi1234 <o.a.elmi@hotmail.com>
2020-12-21 12:08:53 +0000

    Add files via upload

---
+++ Section 3 base features and syntax/Introduction to react hooks useSTATE()/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+ const App = props => {
+  
+  const [personState , setPersonState] = useState({
+   persons : [
+  { name:"Osama", age: 23} , {name:"Aman" , age: 15},  {name: "Amin" , age: 20}
+  ] , 
+
+  otherValue : "Some other value"
+
+  }
+  ); 
+
+const switchNameHandler = () => { setPersonState( { 
+    persons: [
+   
+   { name:"Osama", age: 24} , {name:"Aman" , age: 15}, {name: "Amin" , age: 21}
+             
+             ], 
+
+  otherValue: personState.otherValue
+
+  	}
+  )
+  // in React hooks when manipulating the state , in react hooks you have to manually add other objects such as otherValue as it setting state in hooks repalces the previous state where as in class bassed components you they merge the contents so you dont have to manually add othervalue object 
+  }
+
+   
+   return (
+      <div className="App">
+        <h1>Hi, I'm a React App </h1>
+        <p> Does this work now </p>
+        <button onClick = {switchNameHandler}> Switch Name </button>
+        <Person name = {personState.persons[0].name} age = {personState.persons[0].age}/>
+        <Person  name = {personState.persons[1].name} age = {personState.persons[1].age}>My Hobbies: Playing Games </Person>
+        <Person name =  {personState.persons[2].name} age = {personState.persons[2].age}/>
+      </div>
+    );
+   
+  }
+
+export default App;
+
